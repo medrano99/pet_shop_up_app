cac7b0e534e3380d1cae03aa9aaa1760b7798a79
PC\MEDRANO <alexander99medrano@gmail.com>
2020-09-22 19:19:59 -0500

    up2

--- appWeb/src/java/udem/edu/co/entities/Cliente.java
+++ appWeb/src/java/udem/edu/co/entities/Cliente.java
@@ -37,12 +37,6 @@ import javax.xml.bind.annotation.XmlTransient;
     @NamedQuery(name = "Cliente.findByDireccion", query = "SELECT c FROM Cliente c WHERE c.direccion = :direccion")})
 public class Cliente implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "id_cliente")
-    private Integer idCliente;
     @Size(max = 20)
     @Column(name = "nombre")
     private String nombre;
@@ -55,6 +49,13 @@ public class Cliente implements Serializable {
     @Size(max = 20)
     @Column(name = "direccion")
     private String direccion;
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_cliente")
+    private Integer idCliente;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "fkCliente")
     private List<Factura> facturaList;
 
@@ -73,37 +74,6 @@ public class Cliente implements Serializable {
         this.idCliente = idCliente;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
 
     @XmlTransient
     public List<Factura> getFacturaList() {
@@ -137,6 +107,38 @@ public class Cliente implements Serializable {
     @Override
     public String toString() {
         return "udem.edu.co.entities.Cliente[ idCliente=" + idCliente + " ]";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
     
 }
